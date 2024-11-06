56ece5cb503e4ab6329a3b0e5040b85cdc1b27d1
rik <sergio.certificaciones.estudio@gmail.com>
2024-11-06 14:38:59 -0500

    update deploy 1

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https://github.com/rikz2/u-frontend-profundizacion/'
+  base:'https://rikz2.github.io/u-frontend-profundizacion/'
 })
